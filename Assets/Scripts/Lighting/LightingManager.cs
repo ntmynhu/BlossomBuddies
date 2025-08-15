ae06019faf46afb9efca131eab77900ea6e7f4db
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-08-15 16:01:39 +0700

    Implement day night circle

---
+++ Assets/Scripts/Lighting/LightingManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset lightingPreset;
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+
+    private float updateLightingInterval = 30f; // Update every 60 seconds
+    private float updateLightingTimer = 0f;
+
+    private DateTime currentTime;
+
+    private void Start()
+    {
+        currentTime = DateTime.Now;
+        timeOfDay = currentTime.Hour + currentTime.Minute / 60f + currentTime.Second / 3600f;
+        UpdateLighting(timeOfDay / 24);
+
+        Debug.Log(timeOfDay);
+        updateLightingTimer = 0f; // Reset the timer after updating
+    }
+
+    private void Update()
+    {
+        updateLightingTimer += Time.deltaTime;
+
+        if (updateLightingTimer >= updateLightingInterval)
+        {
+            currentTime = DateTime.Now;
+            timeOfDay = currentTime.Hour + currentTime.Minute / 60f + currentTime.Second / 3600f;
+            UpdateLighting(timeOfDay / 24);
+
+            Debug.Log(timeOfDay);
+            updateLightingTimer = 0f; // Reset the timer after updating
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = lightingPreset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = lightingPreset.FogColor.Evaluate(timePercent);
+
+        directionalLight.color = lightingPreset.DirectionalColor.Evaluate(timePercent);
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3(timePercent * 360f - 90f, 170f, 0));
+    }
+}
---
+++ Assets/Scripts/Lighting/LightingPreset.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightingPreset", menuName = "Scriptable Objects/LightingPreset")]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+}
