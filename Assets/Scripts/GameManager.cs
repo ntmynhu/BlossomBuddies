14c602075e3c22019fa0143b4e4185a4975cd167
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-08-19 16:49:56 +0700

    Lighting and cinemachine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,4 +4,9 @@ public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private GameObject player;
     public GameObject Player => player;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 }
