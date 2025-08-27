ea91993b8ff96cbb6e09f6fd3528ba5c1e03b237
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-08-27 10:05:13 +0700

    update

---
+++ Assets/Scripts/Units/DecorationManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DecorationManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+        }
+    }
+}
