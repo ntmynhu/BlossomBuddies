b688c622403b2863251680bb7d71f2f3baf4db44
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2026-07-05 14:50:09 +0700

    update scissors logic

--- Assets/Scripts/GridManagement/PlacementScissorsState.cs
+++ Assets/Scripts/GridManagement/PlacementScissorsState.cs
@@ -1,11 +1,12 @@
-using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PlacementScissorsState : PlacementBaseState
 {
+    private static readonly HashSet<string> GrassIds = new HashSet<string> { "100", "101", "102" };
+
     private Plant targetPlant;
+    private bool targetIsGrass;
 
     public override void EnterState(PlacementSystem placementSystem)
     {
@@ -38,6 +39,12 @@ public class PlacementScissorsState : PlacementBaseState
 
     public override void TriggerAction(PlacementSystem placementSystem)
     {
+        if (targetIsGrass)
+        {
+            placementSystem.RemoveObjectFromGrid(gridPosition, GridType.EnvironmentGrid);
+            return;
+        }
+
         if (targetPlant != null)
         {
             if (targetPlant.IsDead)
@@ -54,10 +61,24 @@ public class PlacementScissorsState : PlacementBaseState
 
     public override bool CanTriggerAction(PlacementSystem placementSystem)
     {
-        // If there is a plant at the grid position and it belongs to the current grid data (plantgrid)
+        targetPlant = null;
+        targetIsGrass = false;
+
+        // Check for grass in EnvironmentGrid
+        var envGrid = placementSystem.GridDataDictionary[GridType.EnvironmentGrid];
+        if (envGrid.ContainsPosition(gridPosition))
+        {
+            var data = envGrid.GetPlacementData(gridPosition);
+            if (data != null && GrassIds.Contains(data.placedObjectId))
+            {
+                targetIsGrass = true;
+                return true;
+            }
+        }
+
+        // Check for dead/weeded plant in PlantGrid
         if (placementSystem.CurrentSelectedGridData.ContainsPosition(gridPosition))
         {
-            // Get target plant
             GameObject targetObject = placementSystem.GetMainGridPlacedObject(GridType.PlantGrid, gridPosition);
             targetPlant = targetObject != null ? targetObject.GetComponent<Plant>() : null;
 
